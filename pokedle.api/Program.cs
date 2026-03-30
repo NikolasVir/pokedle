a4877497964443683f608a458358c094e15bfd3a
nikolasvir <nikvir@outlook.com>
2026-03-30 19:44:04 +0300

    feat(logging): integrate serilog and spin off requests to log files

--- pokedle.api/Program.cs
+++ pokedle.api/Program.cs
@@ -3,36 +3,57 @@ using Pokedle.Api.Infrastructure;
 using PokeApiNet;
 using Pokedle.Api.Infrastructure.Seeding;
 using Pokedle.Api.GraphQL;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("Default")
-    ?? throw new InvalidOperationException("Connection string 'Default' not found.");
-
-builder.Services.AddDbContext<PokedleContext>(options =>
-    options.UseNpgsql(connectionString)
-           .UseSnakeCaseNamingConvention());
-
-builder.Services.AddSingleton<PokeApiClient>();
-builder.Services.AddScoped<PokeApiSeeder>();
-builder.Services
-    .AddGraphQLServer()
-    .AddQueryType<Query>()
-    .AddMutationType<Mutation>()
-    .AddFiltering()
-    .AddSorting();
-
-var app = builder.Build();
-
-app.MapGraphQL();
-
-if (args.Contains("--seed"))
+try
 {
-    using var scope = app.Services.CreateScope();
-    var seeder = scope.ServiceProvider.GetRequiredService<PokeApiSeeder>();
-    await seeder.SeedAsync();
-    return;
+    var builder = WebApplication.CreateBuilder(args);
+
+    var connectionString = builder.Configuration.GetConnectionString("Default")
+        ?? throw new InvalidOperationException("Connection string 'Default' not found.");
+
+
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .CreateLogger();
+
+    builder.Services.AddSerilog(Log.Logger);
+
+    builder.Services.AddDbContext<PokedleContext>(options =>
+        options.UseNpgsql(connectionString)
+               .UseSnakeCaseNamingConvention());
+
+    builder.Services.AddSingleton<PokeApiClient>();
+    builder.Services.AddScoped<PokeApiSeeder>();
+    builder.Services
+        .AddGraphQLServer()
+        .AddQueryType<Query>()
+        .AddMutationType<Mutation>()
+        .AddFiltering()
+        .AddSorting();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    app.MapGraphQL();
+
+    if (args.Contains("--seed"))
+    {
+        using var scope = app.Services.CreateScope();
+        var seeder = scope.ServiceProvider.GetRequiredService<PokeApiSeeder>();
+        await seeder.SeedAsync();
+        return;
+    }
+
+    app.Run();
 }
-
-app.Run();
-
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
