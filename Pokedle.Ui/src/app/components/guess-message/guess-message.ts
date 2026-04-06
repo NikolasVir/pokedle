d9aa6f33b386d1e0667d7e7678b02042cf99c9fa
nikolasvir <nikvir@outlook.com>
2026-04-06 17:09:11 +0300

    feat: add win message

--- Pokedle.Ui/src/app/components/guess-message/guess-message.ts
+++ Pokedle.Ui/src/app/components/guess-message/guess-message.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GameService } from '../../game.service';
 
 @Component({
   selector: 'app-guess-message',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './guess-message.html',
   styleUrl: './guess-message.css',
 })
-export class GuessMessage {}
+export class GuessMessage {
+  gameService = inject(GameService);
+}
